76854a480afdb1a0e696475f5e6f033e21dbc3de
NAJIB <najib.azmi2019@gmail.com>
2024-10-06 01:50:25 +0100

    EXERCICE2

---
+++ EXERCICE2/EXERCICE2.py
@@ -0,0 +1,7 @@
+import sys
+if(len(sys.argv)>2):
+              a=float(sys.argv[1])
+              b=float(sys.argv[2])
+              print(f'La somme de {a}+{b}est :{a+b}')
+else:
+        print("Impossi")
