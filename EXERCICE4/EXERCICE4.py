1eb5e1331a1b2b6f288efb079848fb332c7beff8
NAJIB <najib.azmi2019@gmail.com>
2024-10-06 13:36:08 +0100

    EXERCICE4 5

---
+++ EXERCICE4/EXERCICE4.py
@@ -0,0 +1,3 @@
+l=float(input("Veuillez saisir la longueur du côté"))
+print(f'le périmètre : {4*l}')
+print(f'l\'aire  : {l*l}')
---
+++ EXERCICE5/EXERCICE5.py
@@ -0,0 +1,7 @@
+list1=['abcsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssde','ab','yui']
+i=0
+list2=[]
+for x in list1 :
+        list2.append(len(x))
+print(list1)   
+print(list2) 
